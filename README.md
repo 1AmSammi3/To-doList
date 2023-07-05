b621de7e05378996ec372328f98706aca7fc1d6f
Sammie <osenisamad2004@gmail.com>
2023-07-05 10:42:33 +0100

    To-do list app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# To-doList
+Todo list app.
+Buggy but still nice.
+Kindly check it out
--- app.js
+++ app.js
@@ -1,37 +1,52 @@
 let input = document.querySelector(".input");
 let listBox = document.querySelector("ul");
+let list, flex, checked, content, discard;
 
 function addNew(){
     if(input.value === ""){
         alert("Enter a task");
     }
     else{
-        let list = document.createElement("li");
-        let flex = document.createElement("div");
-        let checked = document.createElement("span");
-        let content = document.createElement("span");
-        let discard = document.createElement("span");
+        list = document.createElement("li");
+        flex = document.createElement("div");
+        checked = document.createElement("span");
+        content = document.createElement("span");
+        discard = document.createElement("span");
         discard.innerHTML = "\u00d7";
         content.innerHTML = input.value;
         flex.appendChild(checked);
         flex.appendChild(content);
-        flex.appendChild(discard);
+        flex.appendChild(discard);        
         list.appendChild(flex);
         listBox.appendChild(list);
-        flex.setAtrribute("class", "flex");
-        checked.setAtrribute("class", "checked");
-        content.setAtrribute("class", "content");
-        discard.setAtrribute("class", "discard");
+        flex.setAttribute("class", "flex");
+        checked.setAttribute("class", "checked");
+        content.setAttribute("class", "content");
+        discard.setAttribute("class", "discard");
+        checked.setAttribute("onclick", "check()");
+        discard.setAttribute("onclick", "discarded()");
     }
     input.value = "";
 }
 
-listBox.addEventListener("click", function e(){
-    if(e.target.className === "CHECKED"){
-        e.target.innerHTML = "✔️";
-        content.classList.toggle("checked");
+function check() {
+      if(flex) {
+         checked.nextElementSibling.classList.toggle("checked");
+   checked.classList.toggle("check");
+      }
+}
+
+function  discarded(){
+   list.remove();
+}
+
+//discard.addEventListener("click", discarded);
+
+/*flex.addEventListener("click", function e(){
+    if(e.target.tagName === "SPAN"){
+        e.target.classList.toggle("checked");
     }
-    else if(e.target.className === "DISCARD"){
-        e.target.parentElement.remove();
+    if(e.target.innerText === "\u00d7"){
+        e.target.parentElement.parentElement.remove();
     }
-},false);
+}, false);*/
